8d9e783085811f707c35ac3d9b39835bacad0e14
AniketBamane <aniketbamane696@gmail.com>
2024-07-06 16:58:59 +0530

    done with authentication functionality !

---
+++ server/index.js
@@ -0,0 +1,22 @@
+require("dotenv").config()
+const connectToDb = require("./DbConfig/db")
+const express = require('express');
+const cors = require('cors')
+//routers 
+const authRouter = require("./Router/authRouter")
+// const quoteRouter = require("./Router/qouteRouter")
+//middleware
+const errorMiddleware = require("./Middleware/errorMiddleware")
+const app = express();
+app.use(express.json())
+app.use(cors())
+
+app.use("/api/auth", authRouter)
+// app.use("/api/quote", quoteRouter)
+
+app.use(errorMiddleware)
+connectToDb().then(()=>{
+  app.listen(process.env.PORT, () => {
+    console.log(`Example app listening at http://localhost:${process.env.PORT}`)
+  })
+})
