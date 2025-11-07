612db01d36f241bf889b595ba31822fbcfebd7d7
OfficialAniketBamane <138215479+AniketBamane@users.noreply.github.com>
2025-11-07 20:54:58 +0530

    done with solving the email related isue

--- server/Utils/transporter.js
+++ server/Utils/transporter.js
@@ -1,13 +1,16 @@
 const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false, // true for 465, false for other ports
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
-  connectionTimeout: 10000,
-  greetingTimeout: 5000,
-  socketTimeout: 10000,
+  tls: {
+    rejectUnauthorized: false
+  },
+  connectionTimeout: 60000,
+  greetingTimeout: 30000,
+  socketTimeout: 60000,
 });
-
-module.exports = transporter
